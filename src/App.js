ceee23ce0b3d6573a9ab3e7749cf4f33d809d40d
carlosmx45 <carlosjavierfarias@hotmail.com>
2022-07-20 23:56:00 -0500

    se fue a la verga

--- src/App.js
+++ src/App.js
@@ -1,28 +1,109 @@
-import { useState } from "react";
+import { React, useState, useEffect } from "react";
 import Store from "./components/Store";
+import axios from "axios";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//mis Items
 function App() {
+  const [loading, setLoading] = useState(true);
   const [storeItem, setStoreItem] = useState([
     {
-    tittle: "Cartas de Fuego",
+    title: "Cartas de Fuego",
+    color: "Azul",
+    potencia: 7,
     price: 10,
+    image: "https://i.imgur.com/k4PcEsi.png",
     },
     {
-      tittle: "Cartas de Agua",
+      title: "Cartas de Fuego",
+      color: "Morado",
+      potencia: 6,
+      price: 10,
+      image: "https://i.imgur.com/XXWngyc.png",
+    },
+    {
+      title: "Cartas de Fuego",
+      color: "Amarillo",
+      potencia: 2,
+      price: 10,
+      image: "https://i.imgur.com/YW85vmO.png",
+    },
+    {
+      title: "Cartas de Nieve",
+      color: "Rojo",
+      potencia: 2,
+      price: 10,
+      image: "https://i.imgur.com/CquTQxU.png",
+    },
+    {
+      title: "Cartas de Nieve",
+      color: "Naranja",
+      potencia: 3,
+      price: 10,
+      image: "https://i.imgur.com/RtK7sWG.png",
+    },
+    {
+      title: "Cartas de Nieve",
+      color: "Amarillo",
+      potencia: 7,
+      price: 10,
+      image: "https://i.imgur.com/mtPQbXa.png",
+    },
+    {
+      title: "Cartas de Agua",
+      color: "Verde",
+      potencia: 2,
+      price: 10,
+      image: "https://i.imgur.com/mPvXovV.png",
+    },
+    {
+      title: "Cartas de Agua",
+      color: "Morado",
+      potencia: 4,
+      price: 10,
+      image: "https://i.imgur.com/mTXJ8rb.png",
+    },
+    {
+      title: "Cartas de Agua",
+      color: "Azul",
+      potencia: 5,
+      price: 10,
+      image: "https://i.imgur.com/Fovn0Zh.png",
+    },
+    {
+      title: "Cartas de Agua",
+      color: "Amarillo",
+      potencia: 10,
       price: 20,
-    },
-    {
-      tittle: "Cartas de Nieve",
-      price: 30,
+      image: "https://imgur.com/g2jIKoT.png",
     },
   ]);
 
+  useEffect(() => {
+    axios.get('').then(({data}) => {
+      setLoading(false);
+      setStoreItem(data);
+    });
+  }, [storeItem])
+
+  const StoreView = <Store
+    items={storeItem}
+    loading={loading}
+    onItemAdd={itemData => {
+      setStoreItem([...storeItem, itemData])
+    }}
+  />
 
   return (
     <div>
-      <Store items={storeItem} onItemAdd={itemData => {
-        console.log("Data", itemData);
-      }} />
+      <Store
+        items={storeItem}
+        loading={loading}
+        onItemAdd={itemData => {
+          setStoreItem([...storeItem, itemData])
+        }}
+      />
     </div>
   );
 }
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,42 +1,63 @@
-import { useRef } from 'react';
-import { Box, Button, Flex, Heading, Input, Spacer, Stack, Tag } from "@chakra-ui/react";
+import { useRef, useState, } from 'react';
+import { Box, Button, Flex, Center, Heading, Input, Spacer, Stack, Text, Tag, SimpleGrid, GridItem, Image, Spinner } from "@chakra-ui/react";
 
-const StoreItem = ({ title, price }) => {
+import Header from "./Header.js";
+
+//Muestra de Items
+const StoreItem = ({ title, color, potencia, price, image }) => {
     return (
         <Box p={4} borderRadius="lg" borderWidth="1px">
-            <Flex alignItems="center">
-                <Heading size="md">{title}</Heading>
-                <Spacer />
-                <Tag>${price}</Tag>
-            </Flex>
+            <Center>
+                <Image src={image} h="sm" />
+            </Center>
+            <Heading noOfLines={2} size="sm" fontWeight="normal">
+                {title}, Color: {color}.
+            </Heading>
+            <Text>Potencia: {potencia}.</Text>
+            <Tag mt={4}>${price}</Tag>
         </Box>
     );
 };
 
 
-function Store({ items, onItemAdd }) {
-    const itemNameRef = useRef();
-    const itemPriceRef = useRef();
+//referencias para Ingreso de Items
+function Store({ items, loading }) {
+    //Sistema de Visaluzacion de Items
+    const [filteredItems, setFilteredItems] = useState(items);
 
     return (
-        <Box p={4}>
-            <Stack>
-                {items.map((item) => {
-                    return <StoreItem title={item.title} price={item.price} />
-                })}
-           </Stack>
-
-           <Input ref={itemNameRef} mt={10} placeholder="Item name" />
-           <Input ref={itemPriceRef} mt={2} placeholder="Price" type="number" />
-           <Button mt={2} onClick={() => {
-            onItemAdd({
-                title: itemNameRef.current.value,
-                price: itemPriceRef.current.value,
-            });
-           }}> Add Item</Button>
+        <Box>
+            <Header title="Card-Jitsu.Online" />
+            {loading ? (
+                <Center mt={6}>
+                    <Spinner />
+                </Center>
+             ) : (
+                <Box p={2}>
+                    <Input
+                        onChange={(e) => {
+                            let f = items.filter((item) => 
+                                item.title.toLowerCase().includes(e.target.value.toLowerCase())
+                            );
+                            console.log("f" ,f);
+                            setFilteredItems(f);
+                        }} 
+                        placeholder="Buscar"
+                        mt={4}
+                    />
+                    <SimpleGrid columns={5} spacing={4} mt={4} p={2}>
+                        {filteredItems.map((item) => {
+                            return (
+                                <GridItem>
+                                    <StoreItem {...item} />
+                                </GridItem>
+                            )
+                        })}
+                    </SimpleGrid>
+                </Box>
+            )}
         </Box>
     );
 }
-
 
 export default Store;
